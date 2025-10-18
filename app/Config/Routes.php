4ade4e1c6d14ea082fb7c756287b08de56bfd17d
Rosdiyanto <rosdiyantodede@gmail.com>
2025-10-19 00:56:34 +0700

    install dan config shield

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+service('auth')->routes($routes);
+$routes->get('/berhasil-login', 'Berhasil::infoLogin');
+$routes->get('/berhasil-register', 'Berhasil::infoRegister');
---
+++ app/Controllers/Berhasil.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class Berhasil extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+
+    function infoLogin()
+    {
+        echo "berhasil login";
+    }
+
+    function infoRegister()
+    {
+        echo "berhasil register";
+    }
+    
+    
+}
