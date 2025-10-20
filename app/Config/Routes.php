2a5d2600c9b958d76b99d64d2214e35d66b748dc
Rosdiyanto <rosdiyantodede@gmail.com>
2025-10-20 10:33:38 +0700

    routes untuk modular

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,12 +1,17 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
+// use App\Modules\Admin\Controllers\Dashboard;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
 
-service('auth')->routes($routes);
+// service('auth')->routes($routes);
 $routes->get('/berhasil-login', 'Berhasil::infoLogin');
 $routes->get('/berhasil-register', 'Berhasil::infoRegister');
+
+$routes->group('admin', ['namespace' => 'Modules\Admin\Controllers'], function($routes){
+    $routes->get('dashboard', 'Dashboard::index');
+});
