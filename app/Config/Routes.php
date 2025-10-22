f933cc41f0ebddae8e06d6df95cca142f38ac706
Rosdiyanto <rosdiyantodede@gmail.com>
2025-10-22 22:30:34 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,10 +7,27 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
-// service('auth')->routes($routes);
+// bawaan shield
+service('auth')->routes($routes);
+
 $routes->get('/berhasil-login', 'Berhasil::infoLogin');
 $routes->get('/berhasil-register', 'Berhasil::infoRegister');
 
-$routes->group('admin', ['namespace' => 'App\Modules\Admin\Controllers'], function($routes){
-    $routes->get('dashboard', 'Dashboard::index');
+$routes->group('admin', ['namespace' => 'App\Modules\Admin\Controllers', 'filter' => 'group:admin'], function ($routes) {
+    $routes->get('dashboard', 'DashboardController::index');
+    $routes->get('user', 'UserController::index');
 });
+
+$routes->group('datatables', ['namespace' => 'App\Controllers'], function ($routes) {
+    $routes->get('/', 'DatatablesController::index');
+    $routes->get('data', 'DatatablesController::getData');
+    $routes->get('create', 'DatatablesController::create');
+    $routes->post('store', 'DatatablesController::store');
+    $routes->get('edit/(:num)', 'DatatablesController::edit/$1');
+    $routes->post('update/(:num)', 'DatatablesController::update/$1');
+    $routes->get('delete/(:num)', 'DatatablesController::delete/$1');
+    $routes->get('ceking', 'DatatablesController::ceking');
+});
+
+$routes->get('/testemail', 'TestEmail::index');
+
